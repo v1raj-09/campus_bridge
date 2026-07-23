e5dec1906d76c6e954531a1afaa15b93db22f826
Viraj Sawant <sawantviraj605@gmail.com>
2026-07-23 20:49:29 +0530

    Final root package.json configuration

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import path from "path";
 import fs from "fs";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const __dirname = path.resolve();
 const uploadDir = path.join(__dirname, "uploads");
